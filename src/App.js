b2f0e687ab6fe4e6ebe9ef26166ccd20d65c40bf
Ishan Manandhar <ishan02016@gmail.com>
2020-07-12 19:54:46 +0545

    simple custom hook implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [count, newCount] = useState(0);
+  const useCounter = (birth, die, increament) => {
+    const [count, setCount] = useState(birth);
+
+    const handleCount = () => {
+      if (count === die) {
+        alert('You cant live more than 100 years');
+        return setCount(birth);
+      }
+      return setCount(count + increament);
+    };
+    return [count, handleCount];
+  };
+
+  const [count, handleCount] = useCounter(0, 100, 5);
+
   return (
-    <div className="App">
-      <h2>Counter Click</h2>
-      <p>You have clicked {count} times.</p>
-      <button onClick={() => newCount(count + 1)}>Click me</button>
-    </div>
+    <>
+      <div className="App">
+        <h2>How many years do you want to live ?</h2>
+        <p>I want to live {count} years.</p>
+        <br />
+        <div>
+          <button onClick={handleCount}>Click me</button>
+        </div>
+      </div>
+    </>
   );
 }
 
