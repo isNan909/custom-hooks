b5ae0fb9a9df385820e3edefae166326292ebe4d
Ishan Manandhar <ishan02016@gmail.com>
2020-07-12 20:41:43 +0545

    simple custom hook implementation two

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const FetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setResponse(json);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    FetchData();
+    // eslint-disable-next-line
+  }, []);
+  return { response, error };
+};
 
 function App() {
-  const useCounter = (birth, die, increament) => {
-    const [count, setCount] = useState(birth);
+  const res = useFetch(
+    `https://jsonplaceholder.typicode.com/albums/1/photos`,
+    {}
+  );
+  const dataFetched = res.response;
+  // console.log(dataFetched);
 
-    const handleCount = () => {
-      if (count === die) {
-        alert('You cant live more than 100 years');
-        return setCount(birth);
-      }
-      return setCount(count + increament);
-    };
-    return [count, handleCount];
-  };
-
-  const [count, handleCount] = useCounter(0, 100, 5);
-
+  if (!res.response) {
+    return <div>Loading...</div>;
+  }
   return (
-    <>
-      <div className="App">
-        <h2>How many years do you want to live ?</h2>
-        <p>I want to live {count} years.</p>
-        <br />
-        <div>
-          <button onClick={handleCount}>Click me</button>
-        </div>
+    <div className="App">
+      <div>
+        {dataFetched.slice(0, 10).map((data) => (
+          <div key={data.id}>
+            <p>{data.title}</p>
+            <img src={data.url} alt="avatar" />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
