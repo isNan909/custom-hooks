c839a63861d09e975e2d98b0cc9577e82c5a824c
Ishan Manandhar <ishan02016@gmail.com>
2020-07-12 18:48:03 +0545

    simple useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, newCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter Click</h2>
+      <p>You have clicked {count} times.</p>
+      <button onClick={() => newCount(count + 1)}>Click me</button>
     </div>
   );
 }
